7541931c9d97b0d220f37562de311c7b61591c2d
Elfar Morantes <emorantes@edutecno.com>
2023-06-14 00:51:22 -0500

    modificacion vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+/* not allowed baseUrl
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
       ? '././' // prod
       : './', // dev
      }
+*/
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '././' : './',
+  };
